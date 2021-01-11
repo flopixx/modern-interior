46c182ab41f6599a84eaa19eb7df6439a5f3ad50
flopixx <flor.vel96@gmail.com>
2021-01-11 12:51:55 -0300

    agregue el readme

---
+++ app.js
@@ -0,0 +1,31 @@
+const navSile= ()=>{
+const burger=document.querySelector('.meni');
+const wrapper = document.querySelector('.wrapper');
+const caja=document.querySelector('.caja')
+
+
+burger.addEventListener('click', ()=>{
+  
+  
+
+wrapper.classList.toggle('active')
+caja.classList.toggle('none')
+
+
+
+
+
+   
+
+
+
+
+
+
+    
+})
+}
+
+    
+navSile()
+ 
